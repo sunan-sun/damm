aaf177b2d6316459d1ebe81dc0cc314fb41e8ea9
Sunan Sun <sunan@seas.upenn.edu>
2025-08-31 23:51:17 -0400

    target & proposal ratio construction completed; testing awaits in damm

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ class GMM:
         self.mu_0 = mu_0 if mu_0 is not None else np.zeros((self.N,))
         self.kappa_0 = kappa_0 if kappa_0 is not None else 1
 
+        self.index_lists = None
+        self.logProb = None
+        self.Pi = None
 
     def reorder_assignments(self, z):
         M = z.shape[0]
@@ -82,47 +85,67 @@ class GMM:
         prob_cumsum = np.cumsum(postProb, axis=0)
         uniform_draws = np.random.rand(x.shape[0])
         z = np.argmax(prob_cumsum >= uniform_draws, axis=0)
+        self.logProb = logProb
         return z
 
-    def log_proposal_ratio(self, indexList_i, indexList_j, Pi_, components_):
-        """
-        Computes the log proposal ratio for the last Gibbs scan.
-        Args:
-            indexList_i: list of indices for cluster 0
-            indexList_j: list of indices for cluster 1
-            Pi_: array-like, shape (2,)
-            components_: list of two objects, each with .logpdf(x) and .pdf(x) methods
-        Returns:
-            logProposalRatio: float
-        """
-        # Ensure there are only two clusters
-        assert len(Pi_) == 2 and len(components_) == 2, "log_proposal_ratio requires exactly two clusters."
+    def log_proposal_ratio(self):
+        Pi = self.Pi
+        logProb = self.logProb
+        index_lists = self.index_lists
+
         logProposalRatio = 0.0
-        for idx in indexList_i:
-            x_i = self.x[idx]
-            logProposalRatio += np.log(Pi_[0]) + components_[0].logpdf(x_i) - \
-                np.log(Pi_[0] * components_[0].pdf(x_i) + Pi_[1] * components_[1].pdf(x_i))
-        for idx in indexList_j:
-            x_j = self.x[idx]
-            logProposalRatio += np.log(Pi_[1]) + components_[1].logpdf(x_j) - \
-                np.log(Pi_[0] * components_[0].pdf(x_j) + Pi_[1] * components_[1].pdf(x_j))
+        for k in range(2):
+            idx = index_lists[k]
+            # logsumexp over all clusters for each data point in cluster k
+            log_sum = logsumexp(logProb[:, idx], axis=0)
+            # Subtract log Pi[k] and logProb[k, idx] for each point
+            logProposalRatio += np.sum(log_sum - (np.log(Pi[k]) + logProb[k, idx]))
+
         return logProposalRatio
+
+
+    def log_target_ratio(self):
+        Pi = self.Pi
+        logProb = self.logProb
+        index_lists = self.index_lists
+
+        logTargetRatio = 0.0
+        for k in range(2):
+            idx = index_lists[k]
+            logTargetRatio += np.sum((np.log(Pi[k]) + logProb[k, idx]))
+
+
+        index_lists_combined = index_lists[0] + index_lists[1]
+        Psi_n, nu_n, mu_n, kappa_n = self.compute_niw_posterior(self.Psi_0, self.nu_0, self.mu_0, self.kappa_0, self.x[index_lists_combined])
+        Sigma_k = self.sample_inverse_wishart(Psi_n, nu_n)
+        Sigma_k = 0.5 * (Sigma_k + Sigma_k.T)
+        mu_k = self.sample_multi_normal(mu_n, Sigma_k/kappa_n)
+        gaussian_combined = multivariate_normal(mu_k, Sigma_k, allow_singular=True)
+
+        logTargetRatio -= np.sum(gaussian_combined.logpdf(self.x[index_lists_combined]))
+
+
+        return logTargetRatio
+
+
+
 
     def fit(self, T=100):
         z = self.z
         for t in range(T):
             z, K = self.reorder_assignments(z)
-            index_lists = self.update_index_lists(z)
+            self.index_lists = self.update_index_lists(z)
             Pi = np.zeros((K, ))
             gaussian_lists = []
             for k in range(K):
-                Pi[k] = self.rng.gamma(len(index_lists[k]), 1)
-                Psi_n, nu_n, mu_n, kappa_n = self.compute_niw_posterior(self.Psi_0, self.nu_0, self.mu_0, self.kappa_0, self.x[index_lists[k]])
+                Pi[k] = self.rng.gamma(len(self.index_lists[k]), 1)
+                Psi_n, nu_n, mu_n, kappa_n = self.compute_niw_posterior(self.Psi_0, self.nu_0, self.mu_0, self.kappa_0, self.x[self.index_lists[k]])
                 Sigma_k = self.sample_inverse_wishart(Psi_n, nu_n)
                 Sigma_k = 0.5 * (Sigma_k + Sigma_k.T)
                 mu_k = self.sample_multi_normal(mu_n, Sigma_k/kappa_n)
                 gaussian_lists.append(multivariate_normal(mu_k, Sigma_k, allow_singular=True))
             Pi /= np.sum(Pi)
+            self.Pi = Pi
             z = self.sample_label(self.x, Pi, gaussian_lists)
         self.z = z
         return z
