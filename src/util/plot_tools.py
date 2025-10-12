77883bb434ba69a6359596b2cbdfb867ea122160
Sunan Sun <sunan@seas.upenn.edu>
2025-10-11 23:39:38 -0400

    cleaned plot_tools.py

--- src/util/plot_tools.py
+++ src/util/plot_tools.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.lines as mlines
 from matplotlib.ticker import MaxNLocator
 import random
 
@@ -81,114 +80,6 @@ def plot_gmm(x_train, label, damm=None):
         ax.tick_params(axis='z', which='major', pad=15)
         ax.view_init(elev=30, azim=-20)
         # plt.savefig('gmm_result.png', dpi=300)
-
-
-
-def plot_ds_2d(x_train, x_test_list, lpvds, *args):
-    """ passing lpvds object to plot the streamline of DS (only in 2D)"""
-    A = lpvds.A
-    att = lpvds.x_att
-
-    fig = plt.figure(figsize=(16, 10))
-    ax = fig.add_subplot()
-
-    ax.scatter(x_train[:, 0], x_train[:, 1], color='k', s=5, label='original data')
-    for idx, x_test in enumerate(x_test_list):
-        ax.plot(x_test[:, 0], x_test[:, 1], color= 'r', linewidth=2)
-
-    x_min, x_max = ax.get_xlim()
-    y_min, y_max = ax.get_ylim()
-    plot_sample = 50
-    x_mesh,y_mesh = np.meshgrid(np.linspace(x_min,x_max,plot_sample),np.linspace(y_min,y_max,plot_sample))
-    X = np.vstack([x_mesh.ravel(), y_mesh.ravel()])
-    gamma = lpvds.damm.logProb(X.T)
-    for k in np.arange(len(A)):
-        if k == 0:
-            dx = gamma[k].reshape(1, -1) * (A[k] @ (X - att.T))  # gamma[k].reshape(1, -1): [1, num] dim x num
-        else:
-            dx +=  gamma[k].reshape(1, -1) * (A[k] @ (X - att.T)) 
-    u = dx[0,:].reshape((plot_sample,plot_sample))
-    v = dx[1,:].reshape((plot_sample,plot_sample))
-
-    plt.streamplot(x_mesh,y_mesh,u,v, density=3.0, color="black", arrowsize=1.1, arrowstyle="->")
-    plt.gca().set_aspect('equal')
-
-    if len(args) !=0:
-        ax.set_title(args[0])
-
-
-
-
-def plot_ds_3d(x_train, x_test_list):
-    N = x_train.shape[1]
-
-    fig = plt.figure(figsize=(12, 10))
-    if N == 2:
-        ax = fig.add_subplot()
-        ax.scatter(x_train[:, 0], x_train[:, 1], color='k', s=1, alpha=0.4, label="Demonstration")
-        for idx, x_test in enumerate(x_test_list):
-            ax.plot(x_test[:, 0], x_test[:, 1], color= 'b')
-        
-    elif N == 3:
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter(x_train[:, 0], x_train[:, 1], x_train[:, 2], 'o', color='k', s=3, alpha=0.4, label="Demonstration")
-
-        for idx, x_test in enumerate(x_test_list):
-            ax.plot(x_test[:, 0], x_test[:, 1], x_test[:, 2], color= 'b')
-        ax.set_xlabel(r'$\xi_1$', fontsize=38, labelpad=20)
-        ax.set_ylabel(r'$\xi_2$', fontsize=38, labelpad=20)
-        ax.set_zlabel(r'$\xi_3$', fontsize=38, labelpad=20)
-        ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-        ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-        ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-        ax.xaxis.set_major_locator(MaxNLocator(nbins=3))
-        ax.yaxis.set_major_locator(MaxNLocator(nbins=3))
-        ax.zaxis.set_major_locator(MaxNLocator(nbins=3))
-        ax.tick_params(axis='z', which='major', pad=15)
-        ax.axis('equal')
-
-
-
-
-
-
-def plot_incremental_ds(new_data, prev_data, att, x_test_list):
-
-    fig = plt.figure(figsize=(16, 10))
-    ax = fig.add_subplot(projection='3d')
-
-    ax.scatter(prev_data[::4, 0], prev_data[::4, 1], prev_data[::4, 2], color='r', s=10,  label='original data')
-    ax.scatter(new_data[::4, 0], new_data[::4, 1], new_data[::4, 2], color = 'magenta', s=10,  label='new data')
-
-    ax.scatter(att[0, 0], att[0, 1], att[0, 2], marker=(8, 2, 0), s=150, c='k', label='Target')
-
-    new_label = mlines.Line2D([], [], color='red',
-                        linewidth=3, label='Old Demo')
-    old_label = mlines.Line2D([], [], color='magenta',
-                        linewidth=3, label='New Demo')
-    ax.legend(handles=[new_label, old_label])
-
-    L = len(x_test_list)
-    for l in range(L):
-        x_test = x_test_list[l]
-        if l != L - 1:
-            ax.plot3D(x_test[:, 0], x_test[:, 1], x_test[:, 2], 'k', linewidth=3.5)
-        else:
-            ax.plot3D(x_test[:, 0], x_test[:, 1], x_test[:, 2], 'k', linewidth=3.5, label='Reproduction')
-
-    ax.axis('auto')
-    ax.set_xlabel(r'$\xi_1(m)$')
-    ax.set_ylabel(r'$\xi_2(m)$')
-    ax.set_zlabel(r'$\xi_3(m)$')
-    ax.xaxis.set_major_locator(MaxNLocator(nbins=5))
-    ax.yaxis.set_major_locator(MaxNLocator(nbins=5))
-    ax.zaxis.set_major_locator(MaxNLocator(nbins=5))
-    # ax.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
-    # ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
-    # ax.zaxis.set_major_formatter(FormatStrFormatter('%.1f'))
-    ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 
 
 
